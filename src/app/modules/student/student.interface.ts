b2dd0a81603036fc5b8050695b9f23bab9e37ee5
Md Ataullah <ataullahm100@gmail.com>
2024-12-10 22:48:08 +0600

    sds

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,24 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Gurdian = {
+  name: string;
+  phone: string;
+  occupation: string;
+};
+
+export type Student = {
+  id: string;
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  address: string;
+  gurdian: Gurdian;
+  profileImg?: string;
+  isActive: 'active' | 'inactive';
+};
