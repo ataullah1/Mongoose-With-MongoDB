6d5804719264b2e937932e367374fe65695f089f
Md Ataullah <ataullahm100@gmail.com>
2024-12-17 22:57:35 +0600

    solved server run issues

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
-const port = 3000;
+
 // parser-----------
 app.use(express.json());
 app.use(cors());
@@ -12,10 +12,6 @@ app.use('/api/v1/student', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 export default app;
