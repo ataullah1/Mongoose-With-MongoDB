b323ff1d7c88d268e1baa479bd226e1a154263a5
Md Ataullah <ataullahm100@gmail.com>
2024-12-17 23:45:43 +0600

    Update

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use('/api/v1/student', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Server is running 🎋!');
 });
 
 export default app;
