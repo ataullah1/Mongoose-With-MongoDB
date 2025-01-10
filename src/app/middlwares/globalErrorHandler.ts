729b1ab168a664827b8bde81fa9d983f05ed0f32
Md Ataullah <ataullahm100@gmail.com>
2025-01-11 02:31:31 +0600

    Refactor global error handler to improve type safety and reduce unused variable warnings

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -1,16 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // Setting default value---
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Something went wrong';
+
+  const errorSources = [{}];
 
   return res.status(statusCode).json({
     success: false,
